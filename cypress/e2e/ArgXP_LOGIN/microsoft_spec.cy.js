7137c7a73f24ac6ba00fcd0be4ac28af03f176c5
christopherdev1521 <christopherdev1521@gmail.com>
2024-05-10 23:36:34 -0300

    Testing desde pagina principal/home

--- cypress/e2e/ArgXP_LOGIN/microsoft_spec.cy.js
+++ cypress/e2e/ArgXP_LOGIN/microsoft_spec.cy.js
@@ -1,17 +1,31 @@
 describe('Boton Microsoft', () => {
     it('Inicio de sesion con cuenta Microsoft', () => {
 
-         //visita la pagina de acceso a los servicios...
-      cy.visit('https://beta.argxp.com/access');
-
-      //Busca el boton especifico "Microsoft"
-      cy.contains('Microsoft').click();
-
-      //espera a que la ventana emergente de terminos se abra, espera 3 segundos y acepta los terminos mediante el checkbox...
-      cy.get('button[role="checkbox"]').should('have.attr', 'aria-checked', 'false').wait(3000).click();
-
-       // Una vez habilitado el boton habiendo tildado el checkbox, click en acepto y redirige a la pagina de login de cuenta...
-      cy.contains('Acepto').click();
+       // deshabilita uncaught:exception automatico de cypress 
+      cy.on('uncaught:exception', (err, runnable) => {
+        
+        // Devuelve false para indicar que no quieres que la prueba falle automáticamente
+        // Dado que indica error en React App de la web y no de cypress, y cancela la prueba...
+        return false;
+  
+      });
+      // Visita la página principal...
+      cy.visit('https://beta.argxp.com');
+  
+      // Hace clic en el botón "Acceder" en la página principal...
+      cy.contains('Acceder').wait(3000).click();
+  
+      // Espera a que se cargue la página de acceso...
+      cy.url().should('include', '/access');
       
+      // Busca el botón específico "Apple" en la página de acceso y hace clic en él...
+      cy.contains('Microsoft').click(); 
+  
+      // Espera a que se abra la ventana emergente de los terminos, espera 3 segundos y realiza el check.
+      cy.get('button[role="checkbox"]').should('have.attr', 'aria-checked', 'false').wait(3000).click();
+  
+      // Una vez habilitado el botón al haber marcado el checkbox, acepta los terminos.
+      // Esto debería redirigirte a la página de inicio de sesión de cuenta de apple en este caso
+      cy.contains('Acepto').click();
     });
   });
